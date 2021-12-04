7e2ebb1242a989ae808b659ab1537a7f2f45a1eb
Konstantin <kynadonenko@gmail.com>
2021-12-04 11:50:06 +0300

    initial commit

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    private float _direction;
+    [SerializeField] private float speed;
+    public void SetDirection(float direction)
+    {
+        _direction = direction;
+    }
+
+    public void SaySomething()
+    {
+        Debug.Log("Something!");   
+    }
+
+    private void Update()
+    {
+        if (_direction != 0)
+        {
+            var delta = _direction * speed * Time.deltaTime;
+            var newX = transform.position.x + delta;
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField] private Hero _hero;
+
+    public void OnHorizontalMovement(InputAction.CallbackContext context)
+    {
+        var direction = context.ReadValue<float>();
+        _hero.SetDirection(direction);
+    }
+
+    public void OnLeftMouseButtonClick(InputAction.CallbackContext context)
+    {
+        if (context.canceled)
+        {
+            _hero.SaySomething();
+        }
+    }
+}
